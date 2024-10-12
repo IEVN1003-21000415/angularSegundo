884e3595075cffc7e51373b53d09c34d993d8a9d
Dulce Belman <jimedulbelman@gmail.com>
2024-10-11 19:20:51 -0600

    zodiaco 11-10-2024

--- src/app/formularios/zodiaco/zodiaco.component.ts
+++ src/app/formularios/zodiaco/zodiaco.component.ts
@@ -1,61 +1,78 @@
 import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule, FormBuilder } from '@angular/forms'; // Importa ReactiveFormsModule
-import { CommonModule } from '@angular/common'; // Importa CommonModule para *ngIf
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-zodiaco',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './zodiaco.component.html',
-  styleUrl: './zodiaco.component.css'
+  styleUrls: ['./zodiaco.component.css']
 })
+export class ZodiacoComponent {
+  formularioInformacionPersonal: FormGroup;
+  edad: number | null = null;
+  signoZodiacalChino: string | null = null;
+  imagenSignoZodiacalChino: string | null = null;
+  saludo: string | null = null;
 
-    // Objeto que asocia cada signo con su imagen
-    zodiacImages: { [key: string]: string } = {
+  constructor(private fb: FormBuilder) {
+    this.formularioInformacionPersonal = this.fb.group({
+      nombre: ['', Validators.required],
+      apellidoPaterno: ['', Validators.required],
+      apellidoMaterno: ['', Validators.required],
+      dia: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+      mes: ['', [Validators.required, Validators.min(1), Validators.max(12)]],
+      anio: ['', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]],
+      sexo: ['', Validators.required]
+    });
+  }
+
+  calcularEdadYSigno() {
+    const dia = this.formularioInformacionPersonal.get('dia')?.value;
+    const mes = this.formularioInformacionPersonal.get('mes')?.value;
+    const anio = this.formularioInformacionPersonal.get('anio')?.value;
+    const nombre = this.formularioInformacionPersonal.get('nombre')?.value;
+    const apellidoPaterno = this.formularioInformacionPersonal.get('apellidoPaterno')?.value;
+    const apellidoMaterno = this.formularioInformacionPersonal.get('apellidoMaterno')?.value;
+
+    const fechaNacimiento = new Date(anio, mes - 1, dia);
+    const hoy = new Date();
+    this.edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+    const diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth();
+    if (diferenciaMeses < 0 || (diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+      this.edad--;
+    }
+
+    this.signoZodiacalChino = this.obtenerSignoZodiacalChino(anio);
+    this.imagenSignoZodiacalChino = this.obtenerImagenSignoZodiacalChino(this.signoZodiacalChino);
+    this.saludo = `${nombre} ${apellidoPaterno} ${apellidoMaterno}`;
+  }
+
+  obtenerSignoZodiacalChino(anio: number): string {
+    const signosZodiacalesChinos = [
+      'Rata', 'Buey', 'Tigre', 'Conejo', 'Dragon', 'Serpiente', 'Caballo', 'Cabra',
+      'Mono', 'Gallo', 'Perro', 'Cerdo'
+    ];
+    const indice = (anio - 1900) % 12;
+    return signosZodiacalesChinos[indice];
+  }
+
+  obtenerImagenSignoZodiacalChino(signo: string): string {
+    const imagenesZodiacales: { [key: string]: string } = {
       'Rata': 'https://www.clarin.com/img/westernastrology/rata.svg',
       'Buey': 'https://www.clarin.com/img/westernastrology/bufalo.svg',
       'Tigre': 'https://www.clarin.com/img/westernastrology/tigre.svg',
       'Conejo': 'https://www.clarin.com/img/westernastrology/conejo.svg',
-      'Dragón': 'https://www.clarin.com/img/westernastrology/dragon.svg',
+      'Dragon': 'https://www.clarin.com/img/westernastrology/dragon.svg',
       'Serpiente': 'https://www.clarin.com/img/westernastrology/serpiente.svg',
       'Caballo': 'https://www.clarin.com/img/westernastrology/caballo.svg',
       'Cabra': 'https://www.clarin.com/img/westernastrology/cabra.svg',
       'Mono': 'https://www.clarin.com/img/westernastrology/mono.svg',
       'Gallo': 'https://www.clarin.com/img/westernastrology/gallo.svg',
       'Perro': 'https://www.clarin.com/img/westernastrology/perro.svg',
-      'Cerdo': 'https://www.clarin.com/img/westernastrology/chancho.svg'
+      'Cerdo': 'https://link-a-imagen-cerdo'
     };
-  
-    constructor(private fb: FormBuilder) {
-      this.form = this.fb.group({
-        nombre: ['', Validators.required],
-        apaterno: ['', Validators.required],
-        amaterno: ['', Validators.required],
-        dia: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
-        mes: ['', [Validators.required, Validators.min(1), Validators.max(12)]],
-        anio: ['', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]],
-        sexo: ['', Validators.required]
-      });
-    }
-  
-    calcularEdadYSigno() {
-      const { dia, mes, anio } = this.form.value;
-      const birthDate = new Date(anio, mes - 1, dia);
-      const ageDifMs = Date.now() - birthDate.getTime();
-      const ageDate = new Date(ageDifMs);
-      this.age = Math.abs(ageDate.getUTCFullYear() - 1970);
-  
-      // Calcula el signo del horóscopo chino
-      const chineseZodiacSigns = [
-        'Rata', 'Buey', 'Tigre', 'Conejo', 'Dragón', 'Serpiente', 'Caballo', 'Cabra', 'Mono', 'Gallo', 'Perro', 'Cerdo'
-      ];
-      const zodiacIndex = (anio - 4) % 12;
-      this.chineseZodiac = chineseZodiacSigns[zodiacIndex];
-  
-      // Asigna la imagen del signo correspondiente
-      this.zodiacImage = this.zodiacImages[this.chineseZodiac];
-    }
+    return imagenesZodiacales[signo] || '';
   }
-  
-  
 }
